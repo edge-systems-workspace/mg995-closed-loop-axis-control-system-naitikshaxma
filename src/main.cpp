aeb36d1bd53f5f85899d6bd9fe1e8ac4a4459741
naitikshaxma <naitik.sharma_cs.aiml24@gla.ac.in>
2026-02-28 22:38:21 +0530

    commit 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
 #include <Arduino.h>
+#include <Servo.h>
+
+/**
+ * @file main.cpp
+ * @brief MG995 Closed Loop Axis Control
+ * @author Naitik Sharma
+ * @date 2026-02-28
+ */
+
+// TODO 1:
+// Create Servo object
+Servo axisServo;
+
+// TODO 2:
+// Define servo control pin (Use pin 9)
+#define SERVO_PIN 9
+
 void setup() {
-// write your initialization code here
 }
 
-
 void loop() {
-// write your code here
 }
